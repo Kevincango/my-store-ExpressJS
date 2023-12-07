444d9b0a847b3f86bc9d164f02cd9ba80cc1702a
Kevincango <kevincantugomez98@gmail.com>
2023-12-06 22:12:20 -0600

    json response with expressJS and os module to get IP address

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
+const os = require('os');
 const express = require('express');
 const app = express();
 const port = 3000;
+
+function getLocalExternalIP() {
+  const networkInterfaces = os.networkInterfaces();
+  for (let interface in networkInterfaces) {
+    for (let interfaceInfo of networkInterfaces[interface]) {
+      if (interfaceInfo.family === 'IPv4' && !interfaceInfo.internal) {
+        return interfaceInfo.address;
+      }
+    }
+  }
+  return null;
+}
+
 
 app.get('/', (req, res) => {
   res.send('Hello, my first server on Express');
 });
 
+app.get('/kevin', (req, res) => {
+  res.json({
+    name: 'Kevin',
+    age: 25,
+  });
+});
+
 app.listen(port, ()=> {
-  console.log(`server running on port ${port}`);
+  console.log(`http://${getLocalExternalIP()}:${port}`);
 });
 
 
