d27e7baf69b1d596b31db74b669306fb5d0f175d
Kevincango <kevincantugomez98@gmail.com>
2023-12-11 17:00:45 -0600

    receive parameters with get and filtering by id

--- index.js
+++ index.js
@@ -20,12 +20,66 @@ app.get('/', (req, res) => {
   res.send('Hello, my first server on Express');
 });
 
-app.get('/kevin', (req, res) => {
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Product 1',
+      price: 1000,
+    },
+    {
+      name: 'product 2',
+      price: 2000,
+    }
+  ]);
+});
+
+app.get('/products/:id', (req, res)=> {
+  const { id } = req.params;
   res.json({
-    name: 'Kevin',
-    age: 25,
+    id,
+    name: 'product 2',
+    price: 2000,
   });
 });
+
+app.get('/categories/:categoryId/products/:productId', (req, res)=> {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId
+  });
+});
+
+const categories = [
+  {
+    id: 1,
+    name: 'electronics'
+  },
+  {
+    id: 2,
+    name: 'clothes'
+  },
+  {
+    id: 3,
+    name: 'healthcare'
+  }
+];
+
+app.get('/categories', (req, res) => {
+  res.json(categories);
+});
+
+app.get('/categories/:id', (req, res)=> {
+  const { id } = req.params;
+  const category = categories.find(c => c.id == id);
+
+  if(category){
+    res.json(category);
+  }else{
+    res.status(404).send(`category with ID ${id} not found`);
+  }
+})
+
 
 app.listen(port, ()=> {
   console.log(`http://${getLocalExternalIP()}:${port}`);
