0dff40bdbcae9c96299fd82c68f182ddcada519e
Kevincango <kevincantugomez98@gmail.com>
2024-01-31 12:04:19 -0600

    table user creation using migration with sequelize

--- api/libs/sequelize.js
+++ api/libs/sequelize.js
@@ -4,15 +4,12 @@ const setupModels = require('../db/models/index')
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-  dialect: 'mysql',
+  dialect: 'postgres',
   logging: console.log,
 });
 
 setupModels(sequelize);
-
-sequelize.sync();
-
 module.exports = sequelize;
