db345b439d167283172bd292b70b2f012bfe5dfb
Kevincango <kevincantugomez98@gmail.com>
2024-02-14 10:08:40 -0600

    filtering prices with query parameters and sequelize operators

--- api/services/product.service.js
+++ api/services/product.service.js
@@ -1,6 +1,7 @@
 const { faker } = require('@faker-js/faker');
 const { models } = require('../libs/sequelize');
 const  boom = require('@hapi/boom');
+const { Op } = require('sequelize');
 
 class ProductsService{
 
@@ -32,12 +33,27 @@ class ProductsService{
   async allProducts(query){
     const options = {
       include: ['category'],
+      where: {}
     };
     const { limit, offset } = query;
     if(limit && offset){
       options.limit = limit;
       options.offset = offset;
     }
+    const { price } = query;
+
+    if(price){
+      options.where.price = price;
+    }
+
+    const { price_min, price_max} = query;
+    if(price_min && price_max){
+      options.where.price = {
+        [Op.gte]: price_min,
+        [Op.lte]: price_max
+      };
+    }
+
     const products = await models.Product.findAll(options);
     return products;
   }
